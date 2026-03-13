96f33c0090b4241ea3e6b49a9633254bdcf288da
chubinho <andrewchubinets07@yandex.ru>
2026-03-13 17:50:58 +0300

    add logger

---
+++ src/logger.py
@@ -0,0 +1,35 @@
+import logging 
+import sys
+
+
+def set_logger(level: int = logging.INFO, log_file: str = "task.log") -> logging.Logger:
+    """
+    Настройка логирования для платформы
+    
+    Args:
+        level: Уровень логирования
+        log_file: Путь к файлу логов
+    
+    Returns:
+        logging.Logger: Настроенный логгер платформы 
+    """
+    logger = logging.getLogger("task")
+    logger.setLevel(level)
+
+    if logger.handlers:
+        return logger
+    
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        datefmt='%d.%m.%Y %H:%M:%S'
+    )
+
+    file_handler = logging.FileHandler(log_file, encoding='utf-8')
+    file_handler.setLevel(level)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    
+    return logger
+
+logger = set_logger()
